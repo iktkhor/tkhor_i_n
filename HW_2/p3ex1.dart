d83f6903f58ecc439a55908f27b10175c86361c9
iktkhor <txor2009@mail.ru>
2021-10-09 20:17:08 +0300

    add lvl 3 ex 2

---
+++ HW_2/p3ex1.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+int fibonachi(int number) {
+  if (number == 1) {
+    return 0;
+  } else if (number <= 3) {
+    return 1;
+  } else {
+    return fibonachi(number - 1) + fibonachi(number - 2);
+  }
+}
+
+void main() {
+  print(fibonachi(int.parse(stdin.readLineSync()!)));
+}
+
+/*  Рекурсивный метод нахождения чисел Фибоначчи
+  in   out
+  10   34
+
+  15   377
+ */
