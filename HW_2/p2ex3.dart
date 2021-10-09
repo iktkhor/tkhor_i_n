c4fa925c6286cd1b52ad6692eb9eebcd8ef3a6ff
iktkhor <txor2009@mail.ru>
2021-10-09 23:29:15 +0300

    change lvl 2 ex 3

--- HW_2/p2ex3.dart
+++ HW_2/p2ex3.dart
@@ -2,31 +2,22 @@ import 'dart:io';
 
 void main() {
   String input = stdin.readLineSync()!;
-  int k = int.parse(stdin.readLineSync()!);
-  String output = "";
+  String output = "" + input[0];
+  int index = 0;
 
-  for (int i = 0; i < input.length; i++) {
-    if ((input.codeUnitAt(i) - k) < 65) {
-      output += String.fromCharCode(90 - (64 - (input.codeUnitAt(i) - k)));
+  for (int i = 1; i < input.length; i++) {
+    if (input[i] == " ") {
+      if (output[index] == " ") {
+        continue;
+      } else {
+        index++;
+        output += input[i];
+      }
     } else {
-      output += String.fromCharCode(input.codeUnitAt(i) - k);
+      index++;
+      output += input[i];
     }
   }
 
   print(output);
 }
-
-/*
-  in          out
-  XPSE        WORD
-  1
-
-  ZABC        WXYZ
-  3
-
-  RURVOVKX    LOLPIPER
-  6
-
-  XWXKWZVPMZMNWZGWC
-  8
- */
